573596aaa5a4f92c4e29b784b909c035494ab0f5
codify9 <noobcoderhu@gmail.com>
2025-02-01 02:37:20 +0530

    add sort

--- demo.py
+++ demo.py
@@ -1,9 +1,8 @@
-get clone https://github.com/codify9/Pandas.git
-
-
 import pandas as pd
 
 df = pd.read_csv('pokemon_data.csv')
 
-print(df)
+sort = df.sort_values('Name')
 
+# print(sort)
+print(df.iloc[:5])
