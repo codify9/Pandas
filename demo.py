4ed9c351f4d6abe8681d38ef5ef89103192843ec
codify9 <noobcoderhu@gmail.com>
2025-02-01 13:32:41 +0530

    Added a new Total Column.

--- demo.py
+++ demo.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv('pokemon_data.csv')
+# print(df)
 
-sort = df.sort_values('Name')
+# sort = df.sort_values('Name')
 
 # print(sort)
 # print(df.iloc[1])
@@ -18,7 +19,14 @@ sort = df.sort_values('Name')
 # fire_type = df.loc[df['Type 1'] == 'Fire']
 # print(fire_type)
 
-sort_t = df.sort_values(['Type 1', 'HP'], ascending= [1,0])
-print(sort_t)
+# sort_t = df.sort_values(['Type 1', 'HP'], ascending= [1,0])
+# print(sort_t)
 
+# Adding a new coloumn 
 
+df['Total'] = df.iloc[:, 4:10].sum(axis = 1)
+
+cols = list(df.columns.values)
+df = df[cols[0:10] + [cols[-1]] + cols[10:]]
+
+print(df)
