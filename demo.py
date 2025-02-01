5d350280e6803bfbd7f7e1c8673fb7c2f4490d93
codify9 <noobcoderhu@gmail.com>
2025-02-01 13:10:44 +0530

    Added some new functions

--- demo.py
+++ demo.py
@@ -5,4 +5,20 @@ df = pd.read_csv('pokemon_data.csv')
 sort = df.sort_values('Name')
 
 # print(sort)
-print(df.iloc[:5])
+# print(df.iloc[1])
+
+# print(df.iloc[2,1])
+
+# iterarting through each rows
+
+# for index, row in df.iterrows():
+#     print (index, row['Name'])
+
+# Fire type pokemon
+# fire_type = df.loc[df['Type 1'] == 'Fire']
+# print(fire_type)
+
+sort_t = df.sort_values(['Type 1', 'HP'], ascending= [1,0])
+print(sort_t)
+
+
